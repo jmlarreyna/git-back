02d4079de6b1e619145ec74f10b03b7e94270894
Martin <jmlarreyna@gmail.com>
2022-07-17 19:11:45 -0300

    Comentado CORS

--- server.js
+++ server.js
@@ -16,11 +16,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyparser.json())
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//   })
+// );
 
 //Endpoints:
 app.get("/", async (req, res) => {
